56deb741126de7e44bc05f26f4555c6f659d99cc
yomo-dev239 <g1122.svart239@gmail.com>
2025-05-18 07:26:15 +0900

    .lintstagedrc.mjsを修正

--- .lintstagedrc.mjs
+++ .lintstagedrc.mjs
@@ -5,7 +5,7 @@ const buildEslintCommand = (filenames) =>
     .map((f) => path.relative(process.cwd(), f))
     .join(' --file ')}`;
 
-export default {
+module.exports = {
   '*.{js,cjs,mjs,json,ts,tsx,css}': ['prettier --write'],
   '*.{ts,tsx}': ['bash -c tsc --noEmit', buildEslintCommand, 'markuplint'],
 };
